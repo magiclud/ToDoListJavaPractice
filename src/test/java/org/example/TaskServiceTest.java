329caeef08d65d9839d9981e3cb6e0616442a08f
Aga <agnieszka.ludwikowska@aptitudesoftware.com>
2023-05-12 13:33:53 +0200

    clean after review

--- src/test/java/org/example/TaskServiceTest.java
+++ src/test/java/org/example/TaskServiceTest.java
@@ -10,103 +10,83 @@ import java.util.List;
 
 
 public class TaskServiceTest {
-    TaskService taskService;
-    ArrayList<Task> taskList = new ArrayList<>();
+
+    Task task1 = new Task("Zadanie 1", false);
+    Task task2 = new Task("Zadanie 2", true);
+    Task task3 = new Task("Zadanie 3", false);
 
     @Test
     public void shouldAddTaskToTheList() {
-        Task task1 = new Task("Zadanie 1", false);
-        Task task2 = new Task("Zadanie 2", true);
-        Task task3 = new Task("Zadanie 3", false);
+        //Given:
+        ArrayList<Task> taskList = new ArrayList<>(List.of(task1, task2, task3));
+        TaskService taskService = new TaskService(taskList);
         String newTask = "Tidy up your room";
 
-        Given:
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        this.taskService = new TaskService(taskList);
-        int sizeBeforeRemoving = taskService.getAll().size();
-        When:
+        //When:
         taskService.add(newTask);
 
-        Then:
-        assertThat(taskService.getAll().size()).isEqualTo(sizeBeforeRemoving + 1);
+        //Then:
+        assertThat(taskService.getAll().stream().map(Task::value)).containsOnlyOnce(newTask);
     }
 
     @Test
     public void shouldRemoveElementFromList() {
-        Task task1 = new Task("Zadanie 1", false);
-        Task task2 = new Task("Zadanie 2", true);
-        Task task3 = new Task("Zadanie 3", false);
+        //Given:
+        ArrayList<Task> taskList = new ArrayList<>(List.of(task1, task2, task3));
+        TaskService taskService = new TaskService(taskList);
         int indexToRemove = 1;
+        String elementNameToRemove = taskService.getAll().get(indexToRemove).value();
 
-        Given:
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        this.taskService = new TaskService(taskList);
-        int sizeBeforeRemoving = taskService.getAll().size();
-        When:
+        //When:
         taskService.remove(indexToRemove);
-        Then:
-        assertThat(taskService.getAll().size()).isEqualTo(sizeBeforeRemoving - 1);
+
+        //Then:
+        assertThat(taskService.getAll().stream().map(Task::value)).doesNotContain(elementNameToRemove);
     }
 
     @Test
     public void shouldNotRemoveElementsFromListWhenWrongIndexProvided() {
-        Task task1 = new Task("Zadanie 1", false);
-        Task task2 = new Task("Zadanie 2", true);
-        Task task3 = new Task("Zadanie 3", false);
+        //Given:
+        ArrayList<Task> taskList = new ArrayList<>(List.of(task1, task2, task3));
+        TaskService taskService = new TaskService(taskList);
         int indexToRemove = -2;
+        List<Task> elementsBeforeAnAction = taskService.getAll();
 
-        Given:
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        this.taskService = new TaskService(taskList);
-        int sizeBeforeRemoving = taskService.getAll().size();
-        When:
+        //When:
         taskService.remove(indexToRemove);
-        Then:
-        assertThat(taskService.getAll().size()).isEqualTo(sizeBeforeRemoving);
+
+        //Then:
+        assertThat(taskService.getAll()).hasSameElementsAs(elementsBeforeAnAction);
     }
 
     @Test
     public void shouldMarkTaskAsCompleted() {
-        Task task1 = new Task("Zadanie 1", false);
-        Task task2 = new Task("Zadanie 2", true);
-        Task task3 = new Task("Zadanie 3", false);
+        //Given:
+        ArrayList<Task> taskList = new ArrayList<>(List.of(task1, task2, task3));
+        TaskService taskService = new TaskService(taskList);
         int indexOfTheUncompletedTask = 0;
 
-        Given:
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        this.taskService = new TaskService(taskList);
-        When:
+        //When:
         taskService.markAsCompleted(indexOfTheUncompletedTask);
         boolean taskStatusAfterTheChange = taskService.getAll().get(indexOfTheUncompletedTask).completed();
-        Then:
+
+        //Then:
         assertThat(taskStatusAfterTheChange).isTrue();
     }
 
     @Test
     public void shouldNotChangeTheStatusDueToWrongIndex() {
-        Task task1 = new Task("Zadanie 1", false);
-        Task task2 = new Task("Zadanie 2", true);
-        Task task3 = new Task("Zadanie 3", false);
+        //Given:
+        ArrayList<Task> taskList = new ArrayList<>(List.of(task1, task2, task3));
+        TaskService taskService = new TaskService(taskList);
         int indexOfTheUncompletedTask = 3;
+        List<Task> statusesBeforeAction = taskService.getAll();
 
-        Given:
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        this.taskService = new TaskService(taskList);
-        List<Boolean> statusesBeforeAction = taskService.getAll().stream().map(Task::completed).toList();
-        When:
+        //When:
         taskService.markAsCompleted(indexOfTheUncompletedTask);
-        List<Boolean> statusesAfterAction = taskService.getAll().stream().map(Task::completed).toList();
-        Then:
-        assertThat(statusesAfterAction).isEqualTo(statusesBeforeAction);
+
+        //Then:
+        assertThat(taskService.getAll()).usingRecursiveComparison()
+                .isEqualTo(statusesBeforeAction);
     }
 }
